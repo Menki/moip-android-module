c3548f94747a00457ac99575df05f31d13669290
Augusto Souza <augustorsouza@gmail.com>
2011-01-10 21:28:04 -0200

    Criando estrutura inicial para o projeto

---
+++ src/com/menki/moip/Payment.java
@@ -0,0 +1,13 @@
+package com.menki.moip;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Payment extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
