e5d0e715fcd0626e438756b46dc43676d47c9e0b
vitor <vitor@vitor-laptop>
2011-05-12 02:10:35 -0300

    ...

--- src/com/menki/moip/activities/PaymentInfo.java
+++ src/com/menki/moip/activities/PaymentInfo.java
@@ -1,3 +1,32 @@
+/**
+ * Copyright (c) 2011, MENKI MOBILE SOLUTIONS - http://www.menkimobile.com.br
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, 
+ *   this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice, 
+ *   this list of conditions and the following disclaimer in the documentation and/or 
+ *   other materials provided with the distribution.
+ * * Neither the name of the MENKI MOBILE SOLUTIONS nor the names of its contributors 
+ *   may be used to endorse or promote products derived from this software without 
+ *   specific prior written permission.
+ *   
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
+ *  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ *  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
+ *  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR 
+ *  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN 
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF 
+ *  SUCH DAMAGE. 
+ *  
+ *  @version 0.0.1
+ */
 package com.menki.moip.activities;
 
 import java.util.Calendar;
@@ -49,7 +78,10 @@ public class PaymentInfo extends Activity implements OnClickListener
     	setContentView(R.layout.paymentinfo);
     	super.onCreate(savedInstanceState);
     	
+    	this.setViews();
+    	
     	this.pagamentoDireto = getIntent().getParcelableExtra("PagamentoDireto");
+//    	this.pagamentoDireto = (PagamentoDireto) getIntent().getSerializableExtra("PagamentoDireto");
 
 
     	this.value = (TextView)findViewById(R.id.value);
@@ -68,6 +100,7 @@ public class PaymentInfo extends Activity implements OnClickListener
 		{
 		case(R.id.button_next):			
 
+			this.populatePagamentoDireto();
 			Intent intent = new Intent(this,Summary.class);
 			intent.putExtra("PagamentoDireto", this.pagamentoDireto);
 			startActivity(intent);
@@ -124,5 +157,17 @@ public class PaymentInfo extends Activity implements OnClickListener
 		this.nextStepButton = (Button)findViewById(R.id.button_next);
     	this.nextStepButton.setOnClickListener(this);    	
     	
+	}
+	
+	private void populatePagamentoDireto()
+	{
+		if (this.cashRadio.isChecked())
+		{
+			this.pagamentoDireto.setInstallmentsQuantity("1");
+		}
+		else
+		{
+			this.pagamentoDireto.setInstallmentsQuantity(this.installmentsEditText.getText().toString());
+		}
 	}
 }
